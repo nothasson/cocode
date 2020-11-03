b24982e162f0063b190641b19d47cf9ca4c17257
hasson <hassonl@foxmail.com>
2020-11-03 21:12:45 +0800

    一不小心又写了一道水题

---
+++ 104maximum-depth-of-binary-tree.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        return 1 + (l > r ? l : r);
+    }
+}
