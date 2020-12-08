0fe7c6d0507ab861e58436703303d16a5e0e8a31
hasson <hassonl@foxmail.com>
2020-12-08 15:33:00 +0800

    完全平方数

---
+++ 279perfect-squares.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1]; // 默认初始化值都为0
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i; // 最坏的情况就是每次+1
+            for (int j = 1; i - j * j >= 0; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // 动态转移方程
+            }
+        }
+        return dp[n];
+    }
+}
