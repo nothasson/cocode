1c92e1e5287ca0700b70b60c54d33d60c5a8c7de
hasson <hassonl@foxmail.com>
2021-01-03 15:46:42 +0800

    剑指offer的题目

---
+++ 74search-a-2d-matrix.java
@@ -0,0 +1,43 @@
+//从左下角开始找
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+        int i = m - 1;
+        int j = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+}
+
+//直接把二维数组看作是一维的，记得下标转换即可。。
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+
+        // 二分查找
+        int left = 0, right = m * n - 1;
+        int pivotIdx, pivotElement;
+        while (left <= right) {
+            pivotIdx = (left + right) / 2;
+            pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+            if (target == pivotElement) return true;
+            else {
+                if (target < pivotElement) right = pivotIdx - 1;
+                else left = pivotIdx + 1;
+            }
+        }
+        return false;
+    }
+}
