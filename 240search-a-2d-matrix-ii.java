6b96b516d230fbd962eedddfb1d5db32125a7092
hasson <hassonl@foxmail.com>
2021-01-03 15:49:37 +0800

    和74一样

---
+++ 240search-a-2d-matrix-ii.java
@@ -0,0 +1,20 @@
+// 同74
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+        int i = m - 1;
+        int j = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+}
