a98ea92239703ae2caba8d591bd6707a36233fd5
hasson <hassonl@foxmail.com>
2021-01-09 14:55:57 +0800

    甚菜

---
+++ 238product-of-array-except-self.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int k = 1;
+        for (int i = 0; i < res.length; i++) {
+            res[i] = k;
+            k = k * nums[i]; // 此时数组存储的是除去当前元素左边的元素乘积
+        }
+        k = 1;
+        for (int i = res.length - 1; i >= 0; i--) {
+            res[i] *= k; // k为该数右边的乘积。
+            k *= nums[i]; // 此时数组等于左边的 * 该数右边的。
+        }
+        return res;
+    }
+}
